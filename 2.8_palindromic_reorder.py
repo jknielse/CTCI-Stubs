f422f02f82543799b98f30cc49a8f489b519cf5f
Jake Nielsen <jake.k.nielsen@gmail.com>
2015-11-22 15:39:14 -0800

    Solves 2.8

--- 2.8_palindromic_reorder.py
+++ 2.8_palindromic_reorder.py
@@ -5,10 +5,55 @@ from tester import runtests
 # the i'th node of the list will be followed by the N-1-i'th node.
 # Sounds confusing. Is a little. Have a look at the test cases.
 
-def palindromically_reorder(ll):
-    # Your code here
-    pass
+def reverse(ll):
+    if not ll:
+        return None
 
+    prev = None
+    while ll.n:
+        n = ll.n
+        ll.n = prev
+        prev = ll
+        ll = n
+    ll.n = prev
+
+    return ll
+
+
+def ith_node(ll, i):
+    while i > 0:
+        i -= 1
+        ll = ll.n
+
+    return ll
+
+
+def length(ll):
+    counter = 0
+    while ll:
+        ll = ll.n
+        counter += 1
+
+    return counter
+
+def zipll(ll1, ll2):
+    while ll1:
+        next_node = ll1.n
+        if ll2:
+            ll1.n = ll2
+            ll2 = ll2.n
+            ll1.n.n = next_node
+        ll1 = next_node
+
+def palindromically_reorder(ll):
+    l = length(ll)
+    mid_node = ith_node(ll, l/2)
+    if not mid_node:
+        return None
+    second_half = mid_node.n
+    mid_node.n = None
+    rev_second_half = reverse(second_half)
+    zipll(ll, rev_second_half)
 
 tests = [
     [[linkedlist_from_list([1, 2, 3, 4, 5])], linkedlist_from_list([1, 5, 2, 4, 3])],
